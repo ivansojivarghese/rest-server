139e61d4446164dba13e2c3513b19863ceeb6a5c
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2025-12-21 12:21:53 +0800

    Update telemetry.js

--- api/telemetry.js
+++ api/telemetry.js
@@ -51,8 +51,6 @@ export default async function handler(req, res) {
 }
 */
 
-import { neon } from '@neondatabase/serverless';
-
 export default async function handler(req, res) {
   try {
     const sql = neon(process.env.DATABASE_URL);
